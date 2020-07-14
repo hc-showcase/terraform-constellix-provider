4a7cd6aa822966c8a3c8aaa11c1edfe0459f4639
Marc-Steffen Kaesz <mkaesz@hashicorp.local>
2020-07-14 12:24:13 +0200

    added creation of a record

--- main.tf
+++ main.tf
@@ -11,6 +11,19 @@ data "constellix_domain" "domain" {
   name = "msk.pub"
 }
 
+resource "constellix_a_record" "firstrecord" {
+  domain_id     = data.constellix_domain.domain.id
+  source_type   = "domains"
+  ttl           = 100
+  name          = "firstrecord"
+  type          = "A"
+  note cc       = "First record"
+  roundrobin {
+    value        = "5.45.25.35"
+    disable_flag = "false"
+  }
+}
+
 output "domain" {
   value = data.constellix_domain.domain
 }
