7d2c1b081ee8c19e7bfd10b2c8c7090beb7fb86f
Marc-Steffen Kaesz <mkaesz@hashicorp.local>
2020-07-14 11:52:41 +0200

    simple test of the constellix provider

---
+++ main.tf
@@ -0,0 +1,16 @@
+variable "constellix_api_key" {}
+variable "constellix_secret_key" {}
+
+provider "constellix" {
+  apikey    = var.constellix_api_key
+  secretkey = var.constellix_secret_key
+  insecure  = false
+}
+
+data "constellix_domain" "domain1" {
+  name = "msk.pub"
+}
+
+output "domain" {
+  value = data.constellix_domain.domain1
+}
